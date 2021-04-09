d42b79bc4402eb3c44a63bef7bf1caf8220d9ff4
96peterharris <96peterharris@gmail.com>
2021-04-10 00:12:27 +0200

    Connect client backend with gui

---
+++ Client/GUI_Client/GuiFunc.py
@@ -0,0 +1,65 @@
+import sys, path, os, asyncio, threading, socket
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import client
+
+def initClient(self):
+    self.thread = client.Client(parent=self)
+
+def startTh(self):
+    print(self.ipInput.text() + "   " + self.portInput.text())
+    self.thread.set_params(self.ipInput.text(), self.portInput.text())
+    self.thread.start()
+
+def stopTh(self):
+    self.updateStatus("Disconnected")
+    self.thread.stop()
+
+def updateStatus(self, msg):
+    self.statusBar.setText(msg)
+
+def checkInputs(self):
+    print("przed warunkiem")
+    if self.checkPort(self.portInput.text()) is not True and self.checkIp(self.ipInput.text()) is not True:  
+            self.ipInput.setStyleSheet("color: red")
+            self.portInput.setStyleSheet("color: red")      
+            self.portInput.setText("Wrong port number!")
+            self.ipInput.setText("Wrong IP address!")     
+            return False
+    elif self.checkIp(self.ipInput.text()) is not True: 
+            self.ipInput.setStyleSheet("color: red")   
+            self.ipInput.setText("Wrong IP address!")  
+            return False
+    elif self.checkPort(self.portInput.text()) is not True:
+            self.portInput.setStyleSheet("color: red")     
+            self.portInput.setText("Wrong port number!")
+            return False
+    return True
+
+def checkIp(self, ip_addr):
+    def partCheck(ip_addr):
+        try: return str(int(ip_addr)) == ip_addr and 0 <= int(ip_addr) <= 255
+        except: return False
+    if ip_addr.count(".") == 3 and all(partCheck(i) for i in ip_addr.split(".")):
+        return True
+
+def checkPort(self, port):
+    if port == "":
+            return False
+    if all(d.isdigit() for d in port) and (int(port) >= 1 and int(port) <= 65000):
+            return True
+    return False
+
+def checkSocket(self, ip, port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((ip, int(port)))
+            return True
+        except:
+            self.updateStatus("Couldn't connect to server: " + ip + " - " + port)
+            return False
+        finally:
+            s.close()
+           
